6cbac76d7f5167922fa2c67d79adb86ad6099590
Alex Jarabi <jarabialex@gmail.com>
2024-10-26 00:36:38 +0300

    Smaller numbers than current challenge

--- stoney_codes/01-arrays/smallerNumbersThanCurrent.py
+++ stoney_codes/01-arrays/smallerNumbersThanCurrent.py
@@ -53,6 +53,15 @@ def optimal(nums):
 
     return result
 
+# def complex_algorithm(nums):
+#     count = [0] * 102
+#
+#     for num in nums:
+#         count[num+1] += 1
+#     for i in range(1, 102):
+#         count[i] += count[i-1]
+#     return [count[num] for num in nums]
+
 
 if __name__ == "__main__":
     lst = [8,1,2,2,3]
